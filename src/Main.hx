c1ebc535a9e3cf97691508424b2c9c80a850c70b
mrchnk <mrchnk@gmail.com>
2019-01-15 02:44:37 +0300

    setup project

---
+++ src/Main.hx
@@ -0,0 +1,24 @@
+
+
+import openfl.display.Sprite;
+import openfl.Assets;
+import openfl.geom.Rectangle;
+
+class Main extends Sprite {
+
+
+	public function new () {
+
+		super ();
+
+		var scale9 = Assets.getMovieClip ("scale9:Scale9Sprite");
+        untyped scale9.scale9BitmapGrid = new Rectangle(10, 10, 20, 20);
+        scale9.x = 10;
+        scale9.y = 10;
+        scale9.width = 180;
+        scale9.height = 80;
+		addChild (scale9);
+
+	}
+
+}
