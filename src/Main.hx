d98ae5d36443a3d78311c0fbedd8683a297a4bd2
Nikolay Sukharev <mrchnk@gmail.com>
2019-01-15 02:45:54 +0300

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -12,11 +12,11 @@ class Main extends Sprite {
 		super ();
 
 		var scale9 = Assets.getMovieClip ("scale9:Scale9Sprite");
-        untyped scale9.scale9BitmapGrid = new Rectangle(10, 10, 20, 20);
-        scale9.x = 10;
-        scale9.y = 10;
-        scale9.width = 180;
-        scale9.height = 80;
+    scale9.scale9Bitmap = new Rectangle(10, 10, 20, 20);
+    scale9.x = 10;
+    scale9.y = 10;
+    scale9.width = 180;
+    scale9.height = 80;
 		addChild (scale9);
 
 	}
