089d5d31f959e9e274875e06898c5435c63f7abd
mrchnk <mrchnk@gmail.com>
2019-01-16 14:41:40 +0300

    replace tabs

--- src/Main.hx
+++ src/Main.hx
@@ -7,18 +7,18 @@ import openfl.geom.Rectangle;
 class Main extends Sprite {
 
 
-	public function new () {
+  public function new () {
 
-		super ();
+    super ();
 
-		var scale9 = Assets.getMovieClip ("scale9:Scale9Sprite");
+    var scale9 = Assets.getMovieClip ("scale9:Scale9Sprite");
     scale9.scale9Grid = new Rectangle(10, 10, 20, 20);
     scale9.x = 10;
     scale9.y = 10;
     scale9.width = 180;
     scale9.height = 80;
-		addChild (scale9);
+    addChild (scale9);
 
-	}
+  }
 
 }
